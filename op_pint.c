5ca05ba4d2c71768b3e27229eccdbbb62e7c549c
Johnson Adeola <115897765+techydeola@users.noreply.github.com>
2023-08-24 17:41:03 +0100

    Update op_pint.c

--- op_pint.c
+++ op_pint.c
@@ -3,7 +3,7 @@
  * opcode_pint - This is a function that return the printed value at the to
  * of the stack
  * @stack: This is an argument that reprsent the given stack
- * @cmd_line: This is an argument that reprsent the amount of lines passed
+ * @cmd_line: This is an argument that reprsents the amount of lines passed
  *
  * Return: This function return a void
  */
